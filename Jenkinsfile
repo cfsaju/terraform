e5c9cdf129d862a9a4a3dfc1cc94256f94c96e8a
Saju Francis <sajufrancis@Sajus-MacBook-Pro.local>
2025-02-09 16:47:44 +0000

    Fixing the jenkins file

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,9 +1,10 @@
 pipeline {
     agent any
+    
     environment {
-        AWS_REGION = 'us-east-1'
-        TF_VAR_aws_access_key = credentials('AWS_ACCESS_KEY')
-        TF_VAR_aws_secret_key = credentials('AWS_SECRET_KEY')
+        AWS_DEFAULT_REGION = 'us-east-1'
+        AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY')
+        AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_KEY')
     }
 
     stages {
--- main.tf
+++ main.tf
@@ -3,47 +3,47 @@ provider "aws" {
 }
 
 resource "null_resource" "log_start" {
- provisioner "local-exec" {
+  provisioner "local-exec" {
     command = <<EOT
         echo "START: $(date)" > terraform_execution.log
         EOT
-   
- }
+
+  }
 
 }
 
 resource "aws_instance" "webserver" {
-  ami           = "ami-04b4f1a9cf54c11d0"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = concat([ aws_security_group.public_sg.id],[] )
+  ami                    = "ami-04b4f1a9cf54c11d0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = concat([aws_security_group.public_sg.id], [])
 }
 
 resource "aws_security_group" "public_sg" {
-    name = "public_sg"
-    description = "Allow port 80 and 22 for web and ssh access"
-    
+  name        = "public_sg"
+  description = "Allow port 80 and 22 for web and ssh access"
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-    }
 
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-    }
-  
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = -1
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    tags = {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
     Name = "Public Security Group"
   }
 }
@@ -83,10 +83,10 @@ output "public_ip" {
 }
 
 resource "null_resource" "log_stop" {
- provisioner "local-exec" {
+  provisioner "local-exec" {
     command = <<EOT
         echo "START: $(date)" > terraform_execution.log
         EOT
- }
+  }
 
 }
