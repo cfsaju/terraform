8f5e69678fe054fb432b0bd66df1371db065bfed
Saju Francis <sajufrancis@Sajus-MacBook-Pro.local>
2025-03-14 16:26:20 +0000

    Added lb dns ame fix

--- main.tf
+++ main.tf
@@ -81,7 +81,23 @@ resource "aws_lb_target_group_attachment" "lb_tgt_grp1_instances" {
 
 # Create the listener and include the target groups
 
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = module.loadbalancer.lb_arn
+  port = 80
+  protocol = "HTTP"
 
+  default_action {
+    type = "forward"
+    target_group_arn = module.loadbalancer.tgt_grp_arn
+  }
+
+}
+
+output "lb_dns_name" {
+
+  value = module.loadbalancer.lb_dns_name
+  
+}
 
 # Create Route53 and Cloud Front
 
--- modules/loadbalancer/outputs.tf
+++ modules/loadbalancer/outputs.tf
@@ -1,3 +1,13 @@
 output "tgt_grp_arn" {
     value = aws_lb_target_group.web_lb.arn
 }
+
+output "lb_dns_name" {
+    value = aws_lb.alb-web.dns_name
+  
+}
+output "lb_arn" {
+
+    value = aws_lb.alb-web.arn
+  
+}
